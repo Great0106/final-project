a874da25bcf6c260c48b83cd427b30152b4d84f8
Rauf Deriel <raufderiel@gmail.com>
2024-08-24 10:45:33 +0700

    navbar and sidebar

--- main/views.py
+++ main/views.py
@@ -3,14 +3,11 @@ from django.http import HttpResponse
 from .models import Post
 
 def index(request):
-    post = Post.objects.get(id=1)
-    context = {
-        'post' : post,
-    }
+    posts = Post.objects.all()
+    context = dict(
+        posts = posts
+    )
     return render(request, 'main/index.html', context)
-
-# def index(request):
-    return render(request, 'main/index.html')
 
 def post(request, post_id):
     return HttpResponse(f"Post ID {post_id}")
